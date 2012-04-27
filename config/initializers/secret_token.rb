57cf01fa5ea6a93b97b0694508d066fa013e356d
Kazato Sugimoto <uiureo@gmail.com>
2012-04-27 21:45:35 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osirco::Application.config.secret_token = '0f05fe9fead4e1169abfb0af08bb3db66c075e067e347dabdcabb2c4647aad59e3168e710c5295e7e6f41c3f235142edff1642a40138c24c3749f06a4cd32e48'
