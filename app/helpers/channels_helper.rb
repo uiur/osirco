f1bc0847894ddce14fd38df4574aeb8b6f7a51de
Kazato Sugimoto <uiureo@gmail.com>
2012-06-17 16:42:10 +0900

    画像を展開するようにした

--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -1,2 +1,11 @@
 module ChannelsHelper
+  def auto_link_and_expand(text)
+    auto_link text, :html => {:target => '_blank'} do |link|
+      res = link
+      if link =~ /(http[s]?\:\/\/.+(?:jpg|jpeg|gif|png))/
+        res += image_tag link
+      end
+      res
+    end
+  end
 end
